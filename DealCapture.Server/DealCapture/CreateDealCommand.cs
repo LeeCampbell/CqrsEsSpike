adc355f62927fd52b2109b698ae681dbb174c447
Lee Campbell <lee.ryan.campbell@gmail.com>
2015-09-07 14:59:21 +0200

    WIP Adding serverside

---
+++ DealCapture.Server/DealCapture/CreateDealCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DealCapture.Server.DealCreation
+{
+    public sealed class CreateDealCommand
+    {
+        public Guid DealId { get; set; }
+        public string Trader { get; set; }
+        public string Counterparty { get; set; }
+        public List<DealSection> Sections { get; set; }
+    }
+
+    public sealed class DealSection
+    {
+        public string ProductType { get; set; }
+        public decimal Notional { get; set; }
+        public string Direction { get; set; }
+        public DateTime DeliveryFrom { get; set; }
+        public DateTime DeliveryUntil { get; set; }
+    }
+}
